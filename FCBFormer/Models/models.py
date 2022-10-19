7879f095dec90b8cab85328e788ee42f04ca8623
unknown <lekietvn@gmail.com>
2022-10-19 15:46:13 +0700

    trivial

--- FCBFormer/Models/models.py
+++ FCBFormer/Models/models.py
@@ -110,9 +110,9 @@ class ChannelAttention(nn.Module):
         self.avg_pool = nn.AdaptiveAvgPool2d(1)
         self.max_pool = nn.AdaptiveMaxPool2d(1)
 
-        self.fc1   = nn.Conv2d(in_planes, in_planes // 16, 1, bias=False)
-        self.relu1 = nn.ReLU() # nguyên gốc trong paper CBAM, thay bằng GELU dc ko?
-        self.fc2   = nn.Conv2d(in_planes // 16, in_planes, 1, bias=False)
+        self.fc1   = nn.Conv2d(in_planes, in_planes // ratio, 1, bias=False) 
+        self.relu1 = nn.ReLU() 
+        self.fc2   = nn.Conv2d(in_planes // ratio, in_planes, 1, bias=False)
 
         self.sigmoid = nn.Sigmoid()
 
